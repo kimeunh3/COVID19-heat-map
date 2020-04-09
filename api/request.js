6ee2c8b30b529381ce3161b705a378493bce268d
Ashley Kim <ashleykim0506@gmail.com>
2020-04-09 15:35:30 -0600

    made changes to the endpoints

--- api/request.js
+++ api/request.js
@@ -23,7 +23,7 @@ app.get('/cityCovid', requestHandler(async (req) => {
 
 			let ret = db.collection('data').findOne({city: {'$regex': regexCity}, province: {'$regex': regexState}, country: "US"})
 	      	ret.then(function(result){
-	      		console.log(result);
+	      		//console.log(result);
 	      		resolve(result);
 	      	})
 			client.close();
@@ -33,7 +33,7 @@ app.get('/cityCovid', requestHandler(async (req) => {
 }));
 
 
-app.get('/stateCovid', requestHandler(async (req) => {
+app.get('/allStateCovid', requestHandler(async (req) => {
 
 	return new Promise(function(resolve, reject) {
 	    MongoClient.connect(CONNECTION_URL, function (err, client) {
@@ -46,11 +46,47 @@ app.get('/stateCovid', requestHandler(async (req) => {
 		            "_id": "$province",
 		            "confirmed": { $sum: "$confirmed" },
 			        "deaths": { $sum: "$deaths" },
-			        "recovered": { $sum: "$recovered" }
+			        "recovered": { $sum: "$recovered" },
+			        "lastUpdate": {$last: "$lastUpdate"}
+		        }
+
+		    }
+			]).toArray(function(err, result){
+				if (err) throw reject(err);
+	          	resolve(result);
+			});
+			client.close();
+		});
+	}); 
+}));
+
+app.get('/stateCovid', requestHandler(async (req) => {
+	const { state}  = req.query;
+	var regexState = new RegExp(["^", state, "$"].join(""), "i");
+
+	return new Promise(function(resolve, reject) {
+	    MongoClient.connect(CONNECTION_URL, function (err, client) {
+		  	if (err) throw err;
+		  	var db = client.db(Data);
+
+		  	ret = db.collection('data').aggregate([    
+		  	{
+		  		"$match": {
+		  			"province" : regexState
+		  		}
+		  	},
+		    {
+		        "$group": {
+		            "_id": "$province",
+		            "confirmed": { $sum: "$confirmed" },
+			        "deaths": { $sum: "$deaths" },
+			        "recovered": { $sum: "$recovered" },
+			        "lastUpdate": {$last: "$lastUpdate"}
 		        }
 		    }
 			]).toArray(function(err, result){
 				if (err) throw reject(err);
+				//console.log(result)
 	          	resolve(result);
 			});
 			client.close();
@@ -71,7 +107,8 @@ app.get('/usCovid', requestHandler(async (req) => {
 		            "_id": "$country",
 		            "confirmed": { $sum: "$confirmed" },
 			        "deaths": { $sum: "$deaths" },
-			        "recovered": { $sum: "$recovered" }
+			        "recovered": { $sum: "$recovered" },
+			        "lastUpdate": {$last: "$lastUpdate"}
 		        }
 		    }
 			]).toArray(function(err, result){
